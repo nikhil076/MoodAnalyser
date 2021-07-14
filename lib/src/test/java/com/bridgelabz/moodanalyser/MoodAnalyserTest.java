c205b78099ca9760620539efbce05f245a984441
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-14 16:13:38 +0530

    Added the test case to check mood analyser for not SAD message and should return HAPPY

--- lib/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ lib/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -13,4 +13,12 @@ public class MoodAnalyserTest {
         String mood = moodAnalyser.analyseMood("This is a sad message");
         Assert.assertThat(mood, CoreMatchers.is("SAD"));
     }
+	
+	@Test
+	public void ifTestMessageIsNotSad_shouldReturnHappy()
+	{
+		MoodAnalyser analyser = new MoodAnalyser();
+		String mood = analyser.analyseMood("This is any Messaage");
+		Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
+	}
 }
