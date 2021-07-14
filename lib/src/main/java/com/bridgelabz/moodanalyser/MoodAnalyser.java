d27b6d5a73e31a1bcfe10fcdebf03daa0e823239
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-14 16:34:23 +0530

    Refactored to take the mood message in the constructor

--- lib/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ lib/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,8 +1,26 @@
 package com.bridgelabz.moodanalyser;
 
-public class MoodAnalyser {
+public class MoodAnalyser 
+{
+	String message;
+	
+	public MoodAnalyser() 
+	{
+		super();
+	}
+
+	public MoodAnalyser(String message) 
+	{
+		super();
+		this.message = message;
+	}
 
 	public String analyseMood(String message)
+	{
+		this.message = message;
+		return analyseMood();
+	}
+	public String analyseMood()
     {
         if(message.contains("sad"))
         {
