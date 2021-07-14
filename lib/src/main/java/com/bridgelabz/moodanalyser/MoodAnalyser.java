703bf9a4786af2d68a4af8160135cf3a20d7db7e
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-14 15:41:56 +0530

    Added functionality to check the mood using the message

---
+++ lib/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyser {
+
+	public String analyseMood(String message)
+    {
+        if(message.contains("sad"))
+        {
+            return "SAD";
+        }
+        else
+        {
+            return "HAPPY";
+        }
+    }
+}
+
--- lib/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyserMain.java
+++ lib/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyserMain.java
@@ -3,6 +3,7 @@ package com.bridgelabz.moodanalyser;
 public class MoodAnalyserMain {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to mood analyser");
+		MoodAnalyser analyser = new MoodAnalyser();
+		System.out.println(analyser.analyseMood("This is sad message"));
 	}
 }
