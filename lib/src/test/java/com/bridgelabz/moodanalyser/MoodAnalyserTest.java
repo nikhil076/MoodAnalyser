d965a1f66d6e56c264ade2349bda6e2b7ce1cf7c
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-14 15:48:27 +0530

    Added test case to check mood analyser for SAD mood

---
+++ lib/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.moodanalyser;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.moodanalyser.MoodAnalyser;
+
+public class MoodAnalyserTest {
+
+	@Test
+    public void testMoodAnalyser()throws Exception
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a sad message");
+        Assert.assertThat(mood, CoreMatchers.is("SAD"));
+    }
+}
